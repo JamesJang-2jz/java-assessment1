7bdd714bb77cd24d9a09b82bce0b84a2276069ad
James Jang <jhjx92@gmail.com>
2023-06-29 16:25:23 -0400

    finishing integer utils

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -1,25 +1,23 @@
 package com.zipcodewilmington.assessment1.part1;
-
-/**
- * Created by leon on 2/16/18.
- */
 public class IntegerUtils {
 
-
-    /**
-     * @param n integer value input by client
-     * @return the sum of all integers between 0 and not including `n`
-     */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        Integer sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
-
     /**
      * @param n integer value input by client
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        Integer sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        return sum;
     }
 
     /**
@@ -27,6 +25,8 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        Integer result = 0;
+        
+        return result;
     }
 }
