64b6518ce241458491fd8a38770396f8fb1f61ca
James Jang <jhjx92@gmail.com>
2023-07-17 13:41:30 -0400

    is palindrome

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,16 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        Integer count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(input.substring(i,j));
+                if (sb.toString().equals(sb.reverse().toString())){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
